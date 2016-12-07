8d615ae9e3cc1b9eb40901719ccf502a7ff77adf
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-07 17:58:14 -0500

    added some dependencies, fixed server and got it running

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const dotenv = require('dotenv').config();
+'use strict'
+
+// const dotenv = require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+// defining routes for server
 const homeRoute = require('./routes/index');
 const showRoute = require('./routes/show');
 // const ejs = require('ejs');
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,10 +20,10 @@ app.use(logger('dev'));
 // app.set('view engine', 'ejs')
 // app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRoute);
 app.use('/show', showRoute);
 
-app.listen(port, () => console.log('running on port', port));
+app.listen(PORT, () => console.log('running on port', PORT));
