add5a86a58cfb9410bc8848eb368454a5f278277
Nick Taft <taft82@gmail.com>
2016-12-07 14:33:50 -0500

    basid server setup

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
-// const homeRoute = require('./routes/index');
-// const showRoute = require('./routes/show');
-const ejs = require('ejs');
+const homeRoute = require('./routes/index');
+const showRoute = require('./routes/show');
+// const ejs = require('ejs');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger('dev'));
 
-app.set('view engine', 'ejs')
-app.set('views', 'views');
+// app.set('view engine', 'ejs')
+// app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', homeRoute)
-// app.use('/show', showRoute)
+app.use('/', homeRoute);
+app.use('/show', showRoute);
 
-app.listen(port, () => console.log('running on port', port))
+app.listen(port, () => console.log('running on port', port));
