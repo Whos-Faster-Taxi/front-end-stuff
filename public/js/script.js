c17e4e99b0a3fb856cc103415454b5bda2ec736c
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 09:51:48 -0500

    please dont tell me i fucked up

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,5 @@
  console.log("DOM fully loaded and parsed");
 
-<<<<<<< HEAD
-let display = document.getElementById("hook");
-
-let button = document.querySelector('#btn');
-
-button.addEventListener('click', getLocation);
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        hook.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position) {
-    hook.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
-=======
  let display = document.getElementById("hook");
  let btn = document.querySelector('#btn');
 
@@ -35,6 +16,3 @@ function showPosition(position) {
      "<br>Longitude: " + position.coords.longitude;
  }
  btn.addEventListener("click", getLocation);
-
-
->>>>>>> master
