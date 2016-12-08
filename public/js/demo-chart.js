a3c387d1392a4bb0aac340c98646076bd200cdc5
Nick Taft <taft82@gmail.com>
2016-12-08 11:47:29 -0500

    dummy chart with dummy data

--- public/js/demo-chart.js
+++ public/js/demo-chart.js
@@ -1,4 +1,5 @@
-import Chart from 'chart.min.js';
+console.log('chart data loaded');
+
 
 let todaysDate = new Date();
 const months  = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -12,51 +13,65 @@ formattedDate = {
 function listHours(hour, num) {
   hourList = [];
   for (let i = 0; i < num; i++) {
-    let eachHour = hour + i < 11 ? `${hour + i + 1} AM` : `${hour - 11 + i} PM`
+    let eachHour = hour + i < 12 ? `${hour + i + 1} AM` : `${hour - 11 + i} PM`
     hourList.push(eachHour);
   }
   return hourList;
 }
 
-weatherByHour = {
-  conditions: ['sunny', 'sunny', 'partial clouds', 'light rain', 'heavy rain' ],
+const weatherByHour = {
+  conditions: ['sunny', 'sunny', 'partial clouds', 'light rain', 'heavy rain', 'thunderstorms' ],
   temperature: [ 72, 70, 68, 66, 64, 62 ],
 }
-transitData = {
+const transitData = {
   uberPredict: [13, 16, 15, 12, 22, 17, 18 ],
   taxiPredict: [12, 15, 23, 23, 25, 12, 27 ],
 }
 
+function setBarColors(arr) {
+  return arr.map((val) => {
+    color = parseInt(val)
+    return `rgba(${color}, ${color}, ${color}, .8)`
+  });
+}
+
+function createLabels(val, temp, cond) {
+  const hours = listHours(todaysDate.getHours(), val.length)
+  labels = [];
+  for (let i = 0; i < val.length - 1; i++) {
+   labels.push(`${hours[i]}, ${temp[i]} deg, ${cond[i]}`)
+   }
+   return labels
+}
+
+
+let barLabels = createLabels(transitData.uberPredict, weatherByHour.temperature, weatherByHour.conditions)
+
+console.log(barLabels)
+const barColors = setBarColors(weatherByHour.temperature);
+
+console.log(barColors)
+
 var ctx = document.getElementById("transitChart").getContext('2d');
-function createChart (ctx, transitData, weatherByHour) {
-  let transitChart = new Chart(ctx {
-      const times = {
+  let transitChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: listHours(todaysDate.getHours(), 6);
+            labels: barLabels,
             datasets: [{
                 label: 'predicted demand',
                 data: transitData.uberPredict,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                backgroundColor: barColors,
+                borderColor: barColors,
                 borderWidth: 1
             }]
         },
         options: {
+          legend: {
+            display: true,
+            labels: {
+                fontColor: 'rgb(255, 99, 132)'
+            }
+        },
             scales: {
                 yAxes: [{
                     ticks: {
@@ -65,7 +80,4 @@ function createChart (ctx, transitData, weatherByHour) {
                 }]
             }
         }
-      }
     });
-  })
-}
