d2291d94d39d3b7369d7b2ef8728e1e4814cd5df
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 12:07:04 -0500

    added changes to js file

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,19 @@
 console.log("DOM fully loaded and parsed");
 
 let display = document.getElementById("hook");
-let btn = document.querySelector('#btn');
+let btn = document.querySelector('.submit');
 
 function getLocation() {
-   if (navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(showPosition);
-   } else {
-       display.innerHTML = "Geolocation is not supported by this browser.";
-   }
+  if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+     display.innerHTML = "Geolocation is not supported by this browser.";
+}
 }
 
 function showPosition(position) {
-   display.innerHTML = "Latitude: " + position.coords.latitude +
-   "<br>Longitude: " + position.coords.longitude;
+  let inputBar = document.querySelector('.input-location');
+  display.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
 }
 btn.addEventListener("click", getLocation);
