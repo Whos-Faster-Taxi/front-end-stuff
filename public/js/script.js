1c6ac39f51053872ad9d9c83b47bd7aa12801a9f
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 09:02:39 -0500

    added location tracking

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+console.log('shit connected');
+
+
+let display = document.getElementById("hook");
+let button = document.querySelector('#hook');
+button.addEventListener('click', getLocation);
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
