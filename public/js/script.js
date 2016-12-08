2e0606b14c109e1f6a7ebdb38b16912d1ab5bcf1
Synclair Wang <synclairwang11@yahoo.com>
2016-12-08 10:40:16 -0500

    oops forgot to fix a conflict error. shits fixed now tho

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,19 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", (event) => {
 
-  let display = document.getElementById("hook");
-  let button = document.querySelector('#hook');
+console.log("DOM fully loaded and parsed");
 
-  button.addEventListener('click', getLocation);
-  function getLocation() {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-          x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-  }
+let display = document.getElementById("hook");
+let btn = document.querySelector('#btn');
 
-  function showPosition(position) {
-      x.innerHTML = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
-  }
+function getLocation() {
+   if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(showPosition);
+   } else {
+       display.innerHTML = "Geolocation is not supported by this browser.";
+   }
+}
 
-  console.log("DOM fully loaded and parsed");
-});
-=======
- console.log("DOM fully loaded and parsed");
-
- let display = document.getElementById("hook");
- let btn = document.querySelector('#btn');
-
- function getLocation() {
-     if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(showPosition);
-     } else {
-         display.innerHTML = "Geolocation is not supported by this browser.";
-     }
- }
-
- function showPosition(position) {
-     display.innerHTML = "Latitude: " + position.coords.latitude +
-     "<br>Longitude: " + position.coords.longitude;
- }
- btn.addEventListener("click", getLocation);
->>>>>>> 204294b79d9d2e4cc6c712b2b2366846fb7aeac5
+function showPosition(position) {
+   display.innerHTML = "Latitude: " + position.coords.latitude +
+   "<br>Longitude: " + position.coords.longitude;
+}
+btn.addEventListener("click", getLocation);
