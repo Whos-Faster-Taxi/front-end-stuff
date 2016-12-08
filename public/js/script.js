3d01236f96693877e87dd4bc3683260d91037bb1
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 14:16:55 -0500

    added more script stupidity

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 console.log("DOM fully loaded and parsed");
 
 let display = document.getElementById("hook");
-let btn = document.querySelector('.submit');
+let locationLink = document.querySelector('.current-location-text');
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -13,8 +13,13 @@ function getLocation() {
 }
 
 function showPosition(position) {
+  // pointing at the input location
   let inputBar = document.querySelector('.input-location');
+  // should I hide the bar and append a h1 with values?
+
+  // this will affect and change inner HTML of element
   display.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
 }
-btn.addEventListener("click", getLocation);
+
+locationLink.addEventListener("click", getLocation);
