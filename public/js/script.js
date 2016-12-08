b39e785be413f165f38d62ce7340c6a4efef1eaa
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 09:49:18 -0500

    added some changes to location

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,19 @@
 console.log('shit connected');
 
-
 let display = document.getElementById("hook");
-let button = document.querySelector('#hook');
+
+let button = document.querySelector('#btn');
+
 button.addEventListener('click', getLocation);
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        hook.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
+    hook.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
 }
