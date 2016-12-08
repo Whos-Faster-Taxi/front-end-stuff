9a5e4c3a1d0dc317205dfbeb12efa33b0e09a370
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-08 09:17:33 -0500

    dom event listener

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,21 @@
-console.log('shit connected');
+document.addEventListener("DOMContentLoaded", (event) => {
 
+ let display = document.getElementById("hook");
+ let button = document.querySelector('#hook');
 
-let display = document.getElementById("hook");
-let button = document.querySelector('#hook');
-button.addEventListener('click', getLocation);
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
+ button.addEventListener('click', getLocation);
+ function getLocation() {
+     if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(showPosition);
+     } else {
+         x.innerHTML = "Geolocation is not supported by this browser.";
+     }
+ }
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
+ function showPosition(position) {
+     x.innerHTML = "Latitude: " + position.coords.latitude +
+     "<br>Longitude: " + position.coords.longitude;
+ }
+
+ console.log("DOM fully loaded and parsed");
+});
